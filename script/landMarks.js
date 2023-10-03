6254d642fac459c3414c564f9eb71a664748e874
atamayo <atamayoquintero@gmail.com>
2023-10-03 18:59:48 -0500

    fix: improve landMarks

--- script/landMarks.js
+++ script/landMarks.js
@@ -1,68 +1,89 @@
-;(() => {
-  const landmarksNative = [
-    { selector: 'header', ariaEquivalent: 'banner' },
-    { selector: 'nav', ariaEquivalent: 'navigation' },
-    { selector: 'main', ariaEquivalent: 'main' },
-    { selector: 'aside', ariaEquivalent: 'complementary' },
-    { selector: 'footer', ariaEquivalent: 'contentinfo' },
-    { selector: 'form', ariaEquivalent: 'form' },
-    { selector: 'section[aria-labelledby]', ariaEquivalent: 'region' }, // Only if it has an accessible name, for example via aria-labelledby
-    { selector: 'address', ariaEquivalent: 'contentinfo' }, // In some cases, but you have to be careful with its use so as not to confuse navigation.
-  ]
+// ;(() => {
+const landmarksNative = [
+  { selector: 'header', ariaEquivalent: 'banner' },
+  { selector: 'nav', ariaEquivalent: 'navigation' },
+  { selector: 'main', ariaEquivalent: 'main' },
+  { selector: 'footer', ariaEquivalent: 'contentinfo' },
+  { selector: 'form', ariaEquivalent: 'form' },
+]
 
-  const landmarkRoles = [
-    'banner', // Usually the site header container.
-    'navigation', // For navigation areas.
-    'main', // Main content of the page.
-    'complementary', // Complementary content with respect to the main content.
-    'contentinfo', // Information about the main content (for example, the footer).
-    'form', // Form.
-    'search', // Search section of the site.
-    'application', // Web application interface.
-    'region', // Only if the region has an accessible name.
-  ]
+const landmarkRoles = [
+  'banner', // Usually the site header container.
+  'navigation', // For navigation areas.
+  'main', // Main content of the page.
+  'contentinfo', // Information about the main content (for example, the footer).
+  'form', // Form.
+]
 
-  //Find and highlight ARIA landmarks
-  console.log('ARIA Landmark Roles in use:')
-  landmarkRoles.forEach((role) => {
-    const elements = document.querySelectorAll(`[role="${role}"]`)
-    elements.forEach((element) => {
-      highlightElement(element, 'ARIA Landmark: ' + role, 'red', 'top')
-    })
-    console.log(`${role}: ${elements.length} occurrences`)
+let ariaLandmarkCount = 0
+let nativeLandmarkCount = 0
+
+//Find and highlight NATIVE landmarks
+console.log('NATIVE Landmark Roles in use:')
+landmarksNative.forEach((item) => {
+  const elements = document.querySelectorAll(item.selector)
+  elements.forEach((element) => {
+    const firstLabel = highlightElement(element, item.selector, 'red')
+    element.firstLabelWidth = firstLabel.offsetWidth
+    nativeLandmarkCount++
   })
-
-  //Find and highlight NATIVE landmarks
-  console.log(' ')
-  console.log('NATIVE Landmark Roles in use:')
-  landmarksNative.forEach((item) => {
-    const elements = document.querySelectorAll(item.selector)
-    elements.forEach((element) => {
-      highlightElement(element, 'NATIVE Landmark: ' + item.selector, 'green', 'bottom')
-    })
+  if (elements.length !== 0) {
     console.log(`${item.selector}: ${elements.length} occurrences`)
-  })
-
-  //Highlights a given HTML element and adds a label with a specified text.
-  function highlightElement(element, label, bgColor, textPos) {
-    element.style.outline = '2px solid red'
-
-    const positionStyle = window.getComputedStyle(element).position
-
-    if (positionStyle === 'static') {
-      element.style.position = 'relative'
-    }
-
-    const labelElement = document.createElement('span')
-    labelElement.innerText = label
-    labelElement.style.position = 'absolute'
-    labelElement.style.background = bgColor
-    labelElement.style.color = 'white'
-    labelElement.style.padding = '2px 5px'
-    labelElement.style.left = '0'
-    labelElement.style[textPos] = '0'
-    labelElement.setAttribute('aria-hidden', true)
-
-    element.prepend(labelElement)
   }
-})()
+})
+
+console.log(' ')
+//Find and highlight ARIA landmarks
+console.log('ARIA Landmark Roles in use:')
+landmarkRoles.forEach((role) => {
+  const elements = document.querySelectorAll(`[role="${role}"]`)
+  const formattedRole = 'a' + role.charAt(0).toUpperCase() + role.slice(1)
+  elements.forEach((element) => {
+    const additionalLeft = element.firstLabelWidth || 0
+    highlightElement(element, formattedRole, '#00F', additionalLeft) // store the label element
+
+    ariaLandmarkCount++
+  })
+  if (elements.length !== 0) {
+    console.log(`${formattedRole}: ${elements.length} occurrences`)
+  }
+})
+
+// Check and display messages
+if (ariaLandmarkCount === 0 && nativeLandmarkCount === 0) {
+  console.log('No landmarks used in page')
+} else {
+  if (ariaLandmarkCount === 0) {
+    console.log('No ARIA landmarks used in page')
+  }
+  if (nativeLandmarkCount === 0) {
+    console.log('No NATIVE landmarks used in page')
+  }
+}
+
+//Highlights a given HTML element and adds a label with a specified text.
+function highlightElement(element, label, color, additionalLeft = '0') {
+  element.style.outline = `2px solid ${color === '#00F' ? color : 'none'}`
+  element.style.border = `2px solid ${color === 'red' ? color : 'none'}`
+  // element.style.boxShadow  = `0 0 0 2px ${color === '#00F' ? color : 'none'}`
+
+  const positionStyle = window.getComputedStyle(element).position
+
+  if (positionStyle === 'static') {
+    element.style.position = 'relative'
+  }
+
+  const labelElement = document.createElement('span')
+  labelElement.innerText = label
+  labelElement.style.position = 'absolute'
+  labelElement.style.padding = '2px'
+  labelElement.style.top = '0'
+  labelElement.style.left = additionalLeft + 'px'
+  labelElement.style.zIndex = 999
+  labelElement.style.background = color
+  labelElement.style.color = 'white'
+
+  element.prepend(labelElement)
+  return labelElement
+}
+// })()
